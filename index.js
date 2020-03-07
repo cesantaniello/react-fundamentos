5c63eb433f04c1899130e059a8b9414b6c7a5674
cesantaniello <carlos.e.santaniello@gmail.com>
2020-03-07 21:46:13 +0100

    State

--- index.js
+++ index.js
@@ -3,37 +3,48 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-function A(props){
-  return <p>Hola {props.nombre}</p>;
-}
+class Contador extends Component {
+  
+  constructor(props) {
+    super(props);
 
-function B(props){
-  return <p>{props.nombre}: 10</p>;
-}
-
-class MiComponenteDeClase extends Component{
-  render(){
-    return <p>Hola soy de la clase</p>;
-  }
-}
-
-class App extends Component {
-  constructor() {
-    super();
     this.state = {
-      name: 'React'
+      contador: 0
     };
   }
 
-  render() {
-    let nombre = "Carlos";
+  aumentar = () => {
+    this.setState({
+      contador: this.state.contador + 1
+    })
+  }
+
+  render() {    
     return (
       <div>
-        <A nombre={nombre} />
-        <B nombre={nombre} />
-      </div>
-    );
+        <p>{this.state.contador}</p>
+        <button onClick={ this.aumentar}>Aumentar</button>
+      </div>);
+    }
   }
-}
+
+  class App extends Component {
+      constructor() {
+        super();
+        this.state = {
+        name: 'React'
+      };
+    }
+
+    render() {
+      let nombre = "Carlos";
+      return (
+        <div>
+          <Contador />
+        </div>
+      );
+    }
+  }
+
 
 render(<App />, document.getElementById('root'));
