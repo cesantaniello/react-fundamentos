f08e33dd2a9d2e0d1d369f952619660fc0351f2b
cesantaniello <carlos.e.santaniello@gmail.com>
2020-03-07 19:26:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+
+function MiComponente(){
+  return <p>Hola Mundo</p>;
+}
+
+class MiComponenteDeClase extends Component{
+  render(){
+    return <p>Hola soy de la clase</p>;
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <MiComponenteDeClase />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
