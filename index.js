d59ce3ac63b73b50cc7953b4ffce3226683fd8ea
cesantaniello <carlos.e.santaniello@gmail.com>
2020-03-08 21:47:05 +0100

    Modularized

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 
-  function Contador(props){
-    const [contador, setContador] = useState(0);
+import Contador from './Contador';
+
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    let nombre = "Carlos";
     return (
       <div>
-        <p>Conteo: {contador}</p>
-        <button onClick={()=> setContador(contador+1)}>Aumentar</button>
+        <Contador />
       </div>
     );
   }
-
-  class App extends Component {
-      constructor() {
-        super();
-        this.state = {
-        name: 'React'
-      };
-    }
-
-    render() {
-      let nombre = "Carlos";
-      return (
-        <div>
-          <Contador />
-        </div>
-      );
-    }
-  }
+}
 
 
 render(<App />, document.getElementById('root'));
